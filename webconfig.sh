f64fbe33f43de8a903228216e47929ccfc9218b4
alexoom <edgar.ramirez@xoom.com>
2015-03-05 11:52:51 -0600

    First commit

---
+++ webconfig.sh
@@ -0,0 +1,14 @@
+    #Config File for Web environment.
+     sudo apt-get update
+     sudo apt-get install -y apache2
+     sudo apt-get install -y php5 php5-gd php5-mysql libssh2-php
+     sudo apt-get install -y libapache2-mod-php5
+     cd /var/www/html
+     sudo wget http://wordpress.org/latest.tar.gz
+     sudo tar xzvf latest.tar.gz
+     cd wordpress
+     sudo cp wp-config-sample.php wp-config.php
+     sudo sed -i "/DB_HOST/s/'[^']*'/'10.10.10.20'/2" wp-config.php
+     sudo sed -i "/DB_NAME/s/'[^']*'/'wordpress'/2" wp-config.php
+     sudo sed -i "/DB_USER/s/'[^']*'/'lex'/2" wp-config.php
+     sudo sed -i "/DB_PASSWORD/s/'[^']*'/'lex'/2" wp-config.php
